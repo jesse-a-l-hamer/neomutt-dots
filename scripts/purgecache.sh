2a72f703633d016c765d3205d41a35047cbfc999
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-02-07 08:27:58 -0600

    feat: initial commit

---
+++ scripts/purgecache.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+## In KB.
+CACHE_LIMIT=51200
+
+cd "$1" 2>/dev/null || exit
+
+[ "$(du -s . | cut -f1 -d'	')" -lt $CACHE_LIMIT ] && exit
+while IFS= read -r i; do
+    rm "$i"
+    [ "$(du -s . | cut -f1 -d'	')" -lt $CACHE_LIMIT ] && exit
+done <<EOF
+$(find . -type f -exec ls -rt1 {} +)
+EOF
